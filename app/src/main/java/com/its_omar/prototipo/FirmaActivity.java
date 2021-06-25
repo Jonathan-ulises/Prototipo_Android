062afccb68b79c82a3964ade673e03ec563b14af
Jonathan <jonulisanpar@gmail.com>
2021-06-25 16:11:12 -0500

    ERRORES con el canvas, falta la implementacion de limpiar el canvas

--- app/src/main/java/com/its_omar/prototipo/FirmaActivity.java
+++ app/src/main/java/com/its_omar/prototipo/FirmaActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.its_omar.prototipo.controller.MyViewController;
 
 public class FirmaActivity extends AppCompatActivity {
-
+    MaterialButton btn;
+    View v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firma);
 
+        btn = findViewById(R.id.btnRehacer);
+        v = findViewById(R.id.srView);
+
+        btn.setOnClickListener(view -> {
+            MyViewController mv = new MyViewController(this, null);
+            mv.clearAnimation();
+        });
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
--- app/src/main/java/com/its_omar/prototipo/controller/MyViewController.java
+++ app/src/main/java/com/its_omar/prototipo/controller/MyViewController.java
@@ -5,21 +5,54 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyViewController extends View {
+
+    float x = 0;
+    float y = 0;
+    String accion = "accion";
+    Path path = new Path();
+
     public MyViewController(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.WHITE);
+        canvas.drawColor(Color.GRAY);
         @SuppressLint("DrawAllocation") Paint p = new Paint();
+        p.setStyle(Paint.Style.STROKE);
         p.setColor(Color.MAGENTA);
         p.setStrokeWidth(10);
-        canvas.drawLine(20, 0, 20, getHeight(), p);
+        //canvas.drawLine(20, 0, 20, getHeight(), p);
 
+        if(accion.equals("down")) {
+            path.moveTo(x, y);
+        } else if(accion.equals("move")){
+            path.lineTo(x,y);
+        }
+
+        canvas.drawPath(path, p);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = event.getX();
+        y = event.getY();
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            accion = "down";
+        }else if(event.getAction() == MotionEvent.ACTION_MOVE){
+            accion = "move";
+        }
+
+        invalidate();
+
+        return true;
     }
 }
