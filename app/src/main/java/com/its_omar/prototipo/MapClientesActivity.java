33ceb4fdc2708ea2c4573ff8318d414b63c41c5d
Jonathan <jonulisanpar@gmail.com>
2021-07-03 10:18:27 -0500

    Consulta de clientes en mapa
    Se agrego la consulta de cientes en la clase del mapa para generar los
    marcadores.

--- app/src/main/java/com/its_omar/prototipo/MapClientesActivity.java
+++ app/src/main/java/com/its_omar/prototipo/MapClientesActivity.java
@@ -1,17 +1,19 @@
 package com.its_omar.prototipo;
 
-import android.graphics.Bitmap;
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.here.android.mpa.cluster.ClusterLayer;
+import com.google.android.material.snackbar.Snackbar;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.GeoPosition;
-import com.here.android.mpa.common.IconCategory;
 import com.here.android.mpa.common.Image;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.common.PositioningManager;
@@ -19,55 +21,53 @@ import com.here.android.mpa.mapping.AndroidXMapFragment;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.MapObject;
+import com.its_omar.prototipo.api.ServiceRetrofit;
+import com.its_omar.prototipo.api.WebService;
 import com.its_omar.prototipo.model.Cliente_por_visitar;
+import com.its_omar.prototipo.model.Empleado;
+import com.its_omar.prototipo.model.resultClienteService.ClientesJSONResult;
+import com.its_omar.prototipo.model.resultClienteService.Resultado;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.List;
 
-import static com.its_omar.prototipo.model.Constantes.INTENT_ARRAY_COORDENADAS;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.its_omar.prototipo.model.Constantes.INTENT_ID_EMPLEADO;
+
 
 public class MapClientesActivity extends AppCompatActivity {
 
 
     //Mapa enbebido
     private Map mapClientes = null;
-
     private PositioningManager posManager;
 
     //Map Fragment
     private AndroidXMapFragment mapFragment = null;
-
     boolean paused = false;
-
     boolean markadorGenerado = false;
+    protected static List<Cliente_por_visitar> listClientes = new ArrayList<>();
 
-    ArrayList<Cliente_por_visitar> listaClientesUbi;
-
-    private ClusterLayer cl;
+    int idEmpleado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_clientes);
 
+        View view = getCurrentFocus();
 
         Bundle extras = getIntent().getExtras();
-
-        listaClientesUbi = extras.getParcelableArrayList(INTENT_ARRAY_COORDENADAS);
-
-       /* if(listaClientesUbi.isEmpty()) {
-            Log.i("listaC", "esta limpia la lista");
-        } else {
-            Log.i("listaC", "Coordenada: " + listaClientesUbi.get(0).getLat());
-        }*/
+        idEmpleado = extras.getInt(INTENT_ID_EMPLEADO);
 
         initMapa();
 
-
-
-        //mapBinding.mapCl.
     }
 
     /**
@@ -91,15 +91,14 @@ public class MapClientesActivity extends AppCompatActivity {
 
                     posicionarVisitador();
 
-                    posicionarClientes();
+                    cargarClientes();
+                //posicionarClientes();
 
                 } else {
                     Toast.makeText(getApplicationContext(), "ERROR DE MAPA " + error.getDetails(), Toast.LENGTH_LONG).show();
                 }
             }
         });
-
-        //posicionarVisitador();
     }
 
     /**
@@ -198,13 +197,83 @@ public class MapClientesActivity extends AppCompatActivity {
         return markerUser;
     }
 
-    private void posicionarClientes() {
-        ArrayList<MapObject> marcadoresCl = new ArrayList<>();
+    private void cargarClientes(){
+        WebService api = ServiceRetrofit.getInstance().getSevices();
+        api.getClientesEmpleado(new Empleado(idEmpleado)).enqueue(new Callback<ClientesJSONResult>() {
+            @Override
+            public void onResponse(Call<ClientesJSONResult> call, Response<ClientesJSONResult> response) {
 
-        for (Cliente_por_visitar cl: this.listaClientesUbi) {
-            marcadoresCl.add(agregarMarcador(cl.getLat(), cl.getLon(), true));
+                if(response.body().isOk()){
+                    if(response.body() != null){
+                        if(response.body().getResultado() != null){
+
+                            //listClientes = jsonResponse(response.body());
+                            mapClientes.addMapObjects(agregarMarcadoresClientes(jsonResponse(response.body())));
+                            /*adapter.submitList(jsonResponse(response.body()));
+                            adapter.notifyDataSetChanged();
+                            clientesBinding.swipeRfshList.setRefreshing(false);*/
+                        } else {
+                            Toast.makeText(getApplication(), "Sin Cliente Asignados", Toast.LENGTH_SHORT).show();
+                           /* clientesBinding.swipeRfshList.setRefreshing(false);*/
+                        }
+                    }
+                }
+
+            }
+
+            @SuppressLint("ResourceType")
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void onFailure(Call<ClientesJSONResult> call, Throwable t) {
+                Snackbar.make(getCurrentFocus(), "Ha sucedido un error", Snackbar.LENGTH_SHORT)
+                        .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).show();
+                t.printStackTrace();
+            }
+        });
+
+
+    }
+
+    /**
+     * Procesa la respuesta del servidor, esto para respuesta de consulta de clientes por visitar
+     * @param res respuesta del servicio {@link ClientesJSONResult}
+     * @return Lista de clientes por visitar {@link List <Cliente_por_visitar>}
+     */
+    private List<Cliente_por_visitar> jsonResponse(ClientesJSONResult res){
+        List<Cliente_por_visitar> clList = new ArrayList<>();
+
+        for (Resultado r:res.getResultado()) {
+            Cliente_por_visitar cl = new Cliente_por_visitar();
+            cl.setIdCliente(r.getJson().getId_Cliente());
+            cl.setNombre(r.getJson().getNombre());
+            cl.setaPaterno(r.getJson().getaPaterno());
+            cl.setaMaterno(r.getJson().getaMaterno());
+            cl.setIne(r.getJson().getFotoINE());
+            cl.setLat(r.getJson().getLati());
+            cl.setLon(r.getJson().getLongt());
+
+
+            /*Como no recibe estos datos del servidor, se pone "sin .."
+            para que no de errores el DIFF CALL*/
+            cl.setCasa("sin casa");
+            cl.setFirma("sin firma");
+            clList.add(cl);
         }
 
-        mapClientes.addMapObjects(marcadoresCl);
+        return clList;
+    }
+
+    /**
+     * Genrera lista de marcadores de clientes para mostrar en el mapa
+     * @param lsCl Lista de clientes por visitar {@link List<Cliente_por_visitar>}
+     * @return Lista de opjetos de mapa {@link List<MapObject>}
+     */
+    private List<MapObject> agregarMarcadoresClientes(List<Cliente_por_visitar> lsCl) {
+        List<MapObject> listaMarcadores = new ArrayList<>();
+        for(Cliente_por_visitar c : lsCl) {
+            listaMarcadores.add(agregarMarcador(c.getLat(), c.getLon(), true));
+        }
+
+        return listaMarcadores;
     }
 }
